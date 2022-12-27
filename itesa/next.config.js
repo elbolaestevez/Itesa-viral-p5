99330d3502075a858711b65dc6c694fb99ec8e49
elbolaestevez <elbolaestevez@gmail.com>
2022-12-27 16:21:41 -0300

    esmexternals

--- itesa/next.config.js
+++ itesa/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
-}
+  trailingSlash: true,
+  experimental: {
+    esmExternals: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
