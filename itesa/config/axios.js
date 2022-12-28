262d21f580bf0698b8d16453af71441d419ef852
elbolaestevez <elbolaestevez@gmail.com>
2022-12-28 19:40:59 -0300

    test9

--- itesa/config/axios.js
+++ itesa/config/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_PUBLIC_URL ?? "" + "/api";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? `http://${process.env.NEXT_PUBLIC_PUBLIC_URL}` + "/api"
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` + "/api";
 
 const back = axios.create();
 
